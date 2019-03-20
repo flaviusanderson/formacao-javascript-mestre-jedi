6ba71b389fc81f86bb5cb994824b12ca94d5e6ab
Flávius Pereira <flavius.pereira@ufpe.br>
2019-03-20 14:34:04 -0300

    operadores Aritmeticos matematicos

---
+++ operadores/aritmeticos/aritmeticos.js
@@ -0,0 +1,57 @@
+// Soma 
+console.log("Soma#####################");
+var soma = 20 + 20;
+console.log("soma 20 + 20", soma);
+console.log("20" + "20");
+console.log("20" + 20);
+console.log("Flávius " + "Anderson");
+var numStr = "27";
+console.log(typeof numStr);
+console.log(typeof +numStr);
+console.log(true + false + true);
+console.log("10 + null = ", 10 + null);
+console.log("10 + undefined = ", 10 + undefined);
+
+// Subtração
+console.log("Subtração####################");
+var sub = 500 - 330;
+console.log("500 - 330 = ", sub);
+console.log("-120 -30 = ", -120 -30);
+console.log("-120 +30 = ", -120 +30);
+console.log("-30 =", -30);
+console.log("15 - true", 15 - true);
+var strNum = "56";
+console.log("-(strNum = '56')", -strNum);
+console.log("typeof -(strNum = '56')", typeof -strNum);
+
+
+// Multiplicação
+/*
+console.log("Multiplicação##########");
+var mult = 5*20;
+console.log("5 * 20 = ", 5 * 20);
+console.log(' 7 * "3" = ', 7 * "3");
+console.log(' 7 * "tres" = ', 7 * "tres");
+console.log("Precedência: ", 3 + 3 * 2); // Primeiro a multiplicação
+console.log("Precedência: ()", (3 + 3) * 2); // Primeiro a multiplicação
+console.log("3 * 0 = ", 3 * 0);
+console.log("0 * 3 = ", 0 * 3);
+console.log("3 * -2 = ", 3 * -2);
+console.log ("3 * true = ", 3 * true);
+*/
+
+// Divisão
+console.log("Divisão####################");
+var div = 10 / 2;
+console.log("10 / 2 = ", 10 / 2);
+console.log("10 / 0 = ", 10 / 0);
+console.log("-10 / 0 = ", -10 / 0);
+console.log("2 / 3 = ",2 / 3);
+console.log("Infinity / Infinity = ", Infinity / Infinity);
+console.log('10 / "2" = ', 10 / "2")
+console.log('10 / "dois" = ', 10 / "dois")
+
+// Módulo
+console.log("Módulo########################");
+var mod = 9 % 2;
+console.log("9 % 2", 9 % 2);
